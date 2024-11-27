217d868f7a38b8c7e6b6fc541efcb0b340826559
Goran <goran.not@ufl.edu>
2024-11-26 23:08:28 -0500

    chakraui start, bugs still

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,43 @@
 import './App.css';
 import MapComponent from './Map.jsx';
-import { useState } from 'react';
+import { Box, Input, HStack, Select } from "@chakra-ui/react";
+import React, {useState} from 'react';
+
 
 function App() {
   const [country, setCountry] = useState('');
   const [province, setProvince] = useState('');
   const [sortMethod, setSortMethod] = useState('');
-  const handleChange = (event) =>{
+  const handleCountryChange = (event) => 
     setCountry(event.target.value);
-  }
-  const handleChange2 = (event) =>{
+  const handleProvinceChange = (event) => 
     setProvince(event.target.value);
-  }
-  const handleChange3 = (event) =>{
+  const handleSortChange = (event) => 
     setSortMethod(event.target.value);
-  }
   return (
-    <div className="App">
-      <div className="containter">
-        <div className = "menubar">
-            <ul className = "menu-buttons">
-            <select value={country} onChange={handleChange}> 
-              <option value = "Country">Select Country</option>
-            </select>
-            <select value={province} onChange={handleChange2}>
-              <option value = "Province">Select Province (optional)</option>
-            </select>
-            <select value={sortMethod} onChange={handleChange3}>
-              <option value = "sortMethod">Select sort method</option>
-            </select>
-
-            </ul>
-          </div>
-        </div>
-        <div classname ="map-area">
-          <MapComponent />
-        </div>
-      </div>
+    <Box className="App">
+      <HStack spacing = {4} align = "stretch">
+        
+        <Input placeholder="Search Country"
+        value={country}
+        onChange={handleCountryChange}
+        />
+        <Input placeholder="Search Provice (optional)"
+        value={province}
+        onChange={handleProvinceChange}/>
+          <Select placeholder= "Sort Method"
+          value ={sortMethod}
+          onChange ={handleSortChange}
+          >
+          <option value = "MergeSort">Merge Sort</option>
+          <option value = "QuickSort">Quick Sort</option>
+        </Select>
+      
+      </HStack>
+      <Box className="map-area">
+        <MapComponent />
+      </Box>
+    </Box>
   
   );
 }
