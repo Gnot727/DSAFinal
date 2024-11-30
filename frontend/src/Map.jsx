785a9bd53fec81ebc0d777502853f7cb834c059c
jackplo <jackplo53@gmail.com>
2024-11-30 14:35:51 -0500

    Added country selection, geojson source data, and a mapping from geojson to db names

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -1,8 +1,61 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useState } from 'react';
+import countries from "./assets/custom.geo.json";
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 
 const MapComponent = () => {
   const position = [51.505, -0.09]; // Example coordinates for London
+
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [hoveredCountry, setHoveredCountry] = useState(null);
+
+  const highlightFeature = (e) => {
+    let layer = e.target;
+    setHoveredCountry(layer.feature.properties);
+  }
+
+  const resetHighlight = (e) => {
+    setHoveredCountry(null);
+  }
+
+  const clickFeature = (e) => {
+    let layer = e.target;
+    console.log(layer.feature.properties);
+    setSelectedCountry(layer.feature.properties);
+  };  
+  
+  const onEachFeature = (feature, layer) => {
+    layer.on({
+      click: clickFeature,
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+    });
+  }
+
+  const style = (feature) => {
+    let mapStyle = {
+        fillColor: "#4A7856",
+        weight: 1,
+        opacity: 0,
+        color: '#4A7856',
+        fillOpacity: 0
+    };
+
+    if (selectedCountry && feature.properties.iso_a3 === selectedCountry.iso_a3) {
+      mapStyle.fillColor = '#4A7856';
+      mapStyle.color = '#4A7856';
+      mapStyle.weight = 2;
+      mapStyle.opacity = 1;
+      mapStyle.fillOpacity = 0.8;
+    } else if (hoveredCountry && feature.properties.iso_a3 === hoveredCountry.iso_a3) {
+      mapStyle.fillColor = '#888';
+      mapStyle.color = '#444';
+      mapStyle.weight = 2;
+      mapStyle.opacity = 1;
+      mapStyle.fillOpacity = 0.7;
+    }
+
+    return mapStyle;
+  } 
 
   return (
     <MapContainer maxBounds={[[-90, -180], [90, 180]]} center={position} zoom={3} style={{ height: "100%", width: "100%" }}>
@@ -12,6 +65,7 @@ const MapComponent = () => {
         minZoom={3}
         maxZoom={20}      
       />
+      <GeoJSON data={countries} style={style} onEachFeature={onEachFeature} />
     </MapContainer>
   );
 };
